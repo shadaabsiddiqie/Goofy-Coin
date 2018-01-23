5849643bb77647c87447f451642d1a95ba9c9316
shadaabsiddiqie <shadaabsiddiqie@gmail.com>
2018-01-23 08:19:11 +0530

    user tranaction and blockchain structure beta

---
+++ main.py
@@ -0,0 +1,80 @@
+#user trancation blockchain
+#user (privateK publicK name timeMade)
+#Trancation (from to money time_trancation)-->check
+#blockchain
+import crypto
+
+class User():
+
+    """docstring for User."""
+
+    def __init__(self, name):
+        self.name = name
+        self.__privateKey = None
+        self.publicKey = None
+        self.bornTime = None
+        self.noCoins = None
+
+    def create():
+        (self.__privateKey,self.publicKey)=fun()
+
+    def getprivateKey():
+        return __privateKey
+
+class  Trancation():
+
+    """docstring for Trancation."""
+
+    def __init__(self, From ,To , tCoins):
+        self.id = selfincrement
+        self.From = From
+        self.To = To
+        self.tCoins = tCoins
+        self.tTime = None
+        self.tInProgress = 1
+
+    def checkTranaction():
+        # check if tranaction is right #weather it is suffecant
+        return (t f)
+
+    def coinTranaction(goofy,coins):
+        # coin based tranaction which is goofy creating coins
+        goofy.noCoins = coins
+
+    def userTranaction():
+
+        # self.To.noCoins +=cost
+        #
+        # self.From.noCoins -=cost
+        # just tranaction from to with cost
+
+
+
+
+class Blockchain():
+
+    """docstring for Blockchain."""
+    def __init__(self, arg):
+        self.arg = arg
+
+    def main():
+        goofy = User()
+
+        t1 = Trancation()
+        t1.coinTranaction(goofy,123123)
+
+        u1 = User()
+        u1.noCoins = 1000
+
+        u2 = User()
+        u2.noCoins = 100
+
+        t2 = Trancation()
+        t2.userTranaction(u1,u2,tcoins)
+
+
+
+if __name__== "__main__":
+
+    B = Blockchain()
+    B.main()
