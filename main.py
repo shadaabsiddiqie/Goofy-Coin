f8b8f48f36119b3d267f948c0aa5d273c71efc75
shadaabsiddiqie <shadaabsiddiqie@gmail.com>
2018-01-25 02:30:23 +0530

    transactions code bugs fixed

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from Crypto.Signature import PKCS1_v1_5
 from Crypto.Hash import SHA256
 from base64 import b64encode, b64decode
 import time
-import numpy as np
 class User():
 
     """docstring for User."""
@@ -20,7 +19,7 @@ class User():
         self.bornTime = None
         # self.noCoins = None
 
-    def createKey(id):
+    def createKey(self,id):
         '''
         Generate an RSA keypair with an exponent of 65537 in PEM format
         param: bits The key length in bits
@@ -34,7 +33,7 @@ class User():
         self.__privateKey = new_key.exportKey("PEM")
         # return __privateKey, publicKey
 
-    def signTransaction(hashData):
+    def signTransaction(self,hashData):
         key = self.__privateKey
         rsakey = RSA.importKey(key)
         signer = PKCS1_v1_5.new(rsakey)
@@ -49,7 +48,7 @@ class  Transaction():
         self.toPublickey = None
         self.tCoins = None
         self.tTime = None
-        self.tValied = False
+        self.tValied = True
         self.tMessage = None
         self.tHash = None
         self.tSignature = None
@@ -58,7 +57,7 @@ class  Transaction():
     #     # check if transaction is right #weather it is suffecant
     #     return (t f)
 
-    def verify_sign(pub_key, signature, hashData):
+    def verify_sign(self,pub_key, signature, hashData):
         '''
         Verifies with a public key from whom the data came that it was indeed
         signed by their private key
@@ -67,27 +66,27 @@ class  Transaction():
         return: Boolean. True if the signature is valid; False otherwise.
         '''
         # pub_key = open(public_key_loc, "r").read()
-        rsakey = RSA.importKey(pub_key)
+        rsakey = RSA.importKey(self.fromPublickey)
         signer = PKCS1_v1_5.new(rsakey)
         # digest = SHA256.new()
         # # Assumes the data is base64 encoded to begin with
         # digest.update(b64decode(data))
-        if signer.verify(hashData, b64decode(signature)):
+        if signer.verify(self.tHash, b64decode(self.tSignature)):
             return True
         return False
 
-    def coinTransaction(id,goofy,coins):
+    def coinTransaction(self,id,goofy,coins):
         # coin based transaction which is goofy creating coins
-        userTransaction( id , goofy.publicKey, goofy.publicKey , coins ,goofy)
+        self.userTransaction( id , goofy.publicKey, goofy.publicKey , coins ,goofy)
 
-    def createHash(data):
+    def createHash(self,data):
 
         digest = SHA256.new()
         # It's being assumed the data is base64 encoded, so it's decoded before updating the digest
         digest.update(b64decode(data))
         return digest
 
-    def userTransaction( id , fromPublickey , toPublickey , coins ,fromObj):
+    def userTransaction( self,id , fromPublickey , toPublickey , coins ,fromObj):
 
         self.id = id
         self.fromPublickey = fromPublickey
@@ -95,121 +94,122 @@ class  Transaction():
         self.tCoins = coins
         self.tTime = time.ctime() #use time.time() to use time in numbers
         self.tValied = True
-        self.tMessage = fromPublickey + toPublickey + coin + self.tTime
-        self.tHash = createHash(self.tMessage) # Assumes the data is base64 encoded to begin with
+        self.tMessage = b64encode(fromPublickey + toPublickey + str(self.tCoins) + self.tTime)
+        self.tHash = self.createHash(self.tMessage) # Assumes the data is base64 encoded to begin with
         self.tSignature = fromObj.signTransaction(self.tHash)
 
 class Blockchain():
 
     """docstring for Blockchain."""
 
-    def __init__(self, arg):
-        self.arg = arg
+    def __init__(self,):
+       self.no_users = 0
+       self.no_transaction = 0
+       self.allUsers = []
+       self.allTransactions = []
 
-    def condition():
+    def condition(self):
         print "1 ==> coin based Transaction (add money to goofy)"
         print "2 ==> user Transaction (transfer money from one user to another)"
         print "3 ==> list of users "
         print "4 ==> list of Transaction"
+        print "5 ==> add user"
 
-    def getUser(userPublicKey ,allUsers):
-        for user in allUsers:
+    def getUser(self,userPublicKey):
+        for user in self.allUsers:
             if user.publicKey == userPublicKey :
                 return user
 
-    def verifyTranaction(From , To, cost):
+    def verifyTranaction(self,From , To ,cost):
         # see if the transation is on his name (signature)
         # verify if he has money
         ballence_amount = 0
-        for transaction in allTransactions:
-            if(transation.toPublickey == From.publicKey ):
-                if (transaction.verify_sign(transaction.fromPublickey , transaction.tSignature , transaction.tHash)):
-                    if(transaction.tValied == True):
-                        ballence_amount = transaction.tCoins
-        if ballence_amount > cost :
+        for transaction in self.allTransactions:
+            if(transaction.tValied == True and transaction.toPublickey == From.publicKey ):
+                if(transaction.verify_sign(transaction.fromPublickey , transaction.tSignature , transaction.tHash)):
+                    ballence_amount += transaction.tCoins
+        if ballence_amount >= cost :
             return True
         else:
             return False
 
-    def main():
-
-        condition()
-
-        no_users = 0
-        no_transaction = 0
-
+    def main(self):
         goofy = User("Goofy")
-        goofy.createKey(no_users)
-        no_users = no_users + 1
+        goofy.createKey(self.no_users)
+        self.no_users = self.no_users + 1
+        self.allUsers.append(goofy)
 
-        allUsers = []
-        allTransactions = []
+        while True:
+            self.condition()
+            choice = None
+            choice = raw_input()
 
-        allUsers.append(goofy)
-
-
-
-        while true:
-
-            if raw_input() == "1" :
-                # coin based Transaction (add money to goofy)
+            if choice == "1" :
+                coins = int(raw_input("coins to be transfered "))
                 t = Transaction()
-                t.coinTransaction(no_transaction,goofy,coins)
-                allTransactions.append(t)
-                no_transaction = no_transaction + 1
+                t.coinTransaction(self.no_transaction,goofy,coins)
+                self.allTransactions.append(t)
+                self.no_transaction = self.no_transaction + 1
 
-            elif raw_input() == "2" :
+            elif choice == "2" :
                 # user Transaction (transfer money from one user to another)
                 FromId = int(raw_input("transver from"))
                 ToId = int(raw_input("transver to"))
                 coins = int(raw_input("coins to be transfered "))
 
-                if verifyTranaction(allUsers[FromId] , allUsers[ToId], coins):
+                if self.verifyTranaction(self.allUsers[FromId] , self.allUsers[ToId], coins):
+                    print "tranction is verified and proceding"
                     dept_to_pay = coins
-                    for transaction in allTransactions:
-                        if(transaction.tValied == True and transation.toPublickey == allUsers[FromId].publicKey):
+                    N = self.no_transaction
+                    print N
+                    for x in range(N-1, -1, -1):
+                        print x
+                        transaction = self.allTransactions[x]
+                        if(transaction.tValied == True and transaction.toPublickey == self.allUsers[FromId].publicKey):
                             if(transaction.tCoins <= dept_to_pay):
                                 transaction.tValied = False
                                 t = Transaction()
-                                t.userTransaction( id , allUsers[FromId].publicKey,allUsers[ToId].publicKey , transaction.tCoins ,allUsers[FromId])
-                                allTransactions.append(t)
-                                no_transaction = no_transaction + 1
+                                t.userTransaction( self.no_transaction, self.allUsers[FromId].publicKey,self.allUsers[ToId].publicKey , transaction.tCoins ,self.allUsers[FromId])
+                                self.allTransactions.append(t)
+                                self.no_transaction = self.no_transaction + 1
                                 dept_to_pay = dept_to_pay - transaction.tCoins
                             else:
                                 transaction.tValied = False
                                 t1 = Transaction()
-                                t1.userTransaction(no_transaction,allUsers[FromId].publicKey,allUsers[ToId].publicKey , dept_to_pay ,allUsers[FromId])
-                                allTransactions.append(t1)
-                                no_transaction = no_transaction + 1
+                                t1.userTransaction(self.no_transaction,self.allUsers[FromId].publicKey,self.allUsers[ToId].publicKey , dept_to_pay ,self.allUsers[FromId])
+                                self.allTransactions.append(t1)
+                                self.no_transaction = self.no_transaction + 1
                                 t2 = Transaction()
-                                remaning_balance = transaction.tCost-dept_to_pay
-                                t2.userTransaction(no_transaction,allUsers[FromId].publicKey,allUsers[FromId].publicKey,remaning_balance,allUsers[FromId])
+                                remaning_balance = transaction.tCoins - dept_to_pay
+                                t2.userTransaction(self.no_transaction,self.allUsers[FromId].publicKey,self.allUsers[FromId].publicKey,remaning_balance,self.allUsers[FromId])
                                 dept_to_pay = 0
-                                allTransactions.append(t2)
-                                no_transaction = no_transaction + 1
+                                self.allTransactions.append(t2)
+                                self.no_transaction = self.no_transaction + 1
+                                break;
 
                     # t = Transaction()
                     # t.userTransaction( id , allUsers[FromId].publicKey,allUsers[ToId].publicKey , coins ,allUsers[FromId])
+                print "trancation compleated :-)"
 
-
-            elif raw_input() == "3" :
+            elif choice == "3" :
                 # list of users
-                for user in allUsers:
-                    print "user id ==> "+ user.id +"user name ==>" + user.name
+                for user in self.allUsers:
+                    print "user id ==> "+ str(user.id) +" user name ==>" + user.name
 
-            elif raw_input() == "4" :
+            elif choice == "4" :
                 # list of Transaction
-                for transaction in allTransactions:
-                    From = getUser(transaction.fromPublickey,allUsers)
-                    To = getUser(transaction.toPublickey,allUsers)
-                    print "Transaction was from "+From.name+" to "+To.name+"of worth"+transaction.tCoins
+                for transaction in self.allTransactions:
+                    From = self.getUser(transaction.fromPublickey)
+                    To = self.getUser(transaction.toPublickey)
+                    print "Transaction was from "+From.name+" to "+To.name+" of worth "+ str(transaction.tCoins) +" and this block's validity is " + str(transaction.tValied)
 
-            elif raw_input() == "5":
+            elif choice == "5":
                 # add user
-                name = raw_input("Name of the user")
+                name = raw_input("Name of the user ")
                 u = User(name)
-                u.createKey(no_users)
-                no_users = no_users + 1
+                u.createKey(self.no_users)
+                self.allUsers.append(u)
+                self.no_users = self.no_users + 1
 
 
 
